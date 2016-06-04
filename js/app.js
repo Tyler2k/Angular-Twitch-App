7b6ce08f790ecb4c1ae7e86d676bbf59b5e0e417
Tyler2k <thpearso@asu.edu>
2016-06-04 16:33:20 -0700

    syntax fix

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ var twitchApp = angular.module("twitchApp", ["ngRoute", "ngResource"]);
 
 twitchApp.controller("mainController", ["$scope", "$resource", "$routeParams", "$filter", function ($scope, $resource, $routeParams, $filter) {
 
-   $scope.channel = [{name:"BobRoss"},{name:"OgamingSC2"}, {name: "NALCS1"}, {name:"PokerCentral"}, {name:"DansGaming"}, {name:"Resttpowered"}, {name:"Summit1g"}, {name:"Steel_Tv"}, {name:"ProfessorBroman"}, {name:"StreamerHouse"}, {name:"DotaMajor"}, {name:"Stewie2K"}];
+   $scope.channel = [{name:"BobRoss"},{name:"OgamingSC2"}, {name: "NALCS1"}, {name:"PokerCentral"}, {name:"DansGaming"}, {name:"Resttpowered"}, {name:"Summit1g"}, {name:"Steel_Tv"}, {name:"ProfessorBroman"}, {name:"StreamerHouse"}, {name:"DotaMajor"}, {name:"Stewie2K"}, {name:"nl_Kripp"}, {name: "JoshOG"},{name:"MineskiTV"}, {name:"AnneMunition"}];
 
    $scope.streamAPI = $resource("https://api.twitch.tv/kraken/streams/:id", {callback: "JSON_CALLBACK" }, {get: {method: "JSONP"}});
    $scope.channelAPI = $resource("https://api.twitch.tv/kraken/channels/:id", { callback: "JSON_CALLBACK" }, {get: { method: "JSONP" }});  
